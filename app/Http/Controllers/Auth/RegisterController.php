e86d0d81774878530dabca462e4804a911f6f4ce
Diego Molina <molinacrespo@gmail.com>
2019-07-16 12:05:24 +0200

    Registro de usuarios -> Validación de datos

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,11 +49,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'name' =>  ['required','string','max:100'],//'required|string|max:255',
+            'first_name' =>  ['required','string','max:100'],//'required|string|max:255',
+            'last_name' =>  ['required', 'string','max:100'],//'required|string|max:255',
+            'email' =>  ['required', 'email', 'unique:users'],//'required|string|email|max:255|unique:users',
+            'password' =>  ['required','min:6','confirmed'],//'required|string|min:6|confirmed',
         ]);
     }
 
--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -3,9 +3,8 @@
 namespace Tests\Feature;
 
 use App\User;
-use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RegistrationTest extends TestCase
@@ -38,6 +37,123 @@ class RegistrationTest extends TestCase
         $this->assertTrue(
             Hash::check('secret', User::first()->password),
             'The password must be hashed'
+        );
+    }
+
+    /** @test */
+    function the_name_is_required()
+    {
+        $userData = $this->userValidData(['name' => null]);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['name']);
+    }
+
+    /** @test */
+    function the_name_must_be_string()
+    {
+        $userData = $this->userValidData(['name' => 1234]);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['name']);
+    }
+
+    /** @test */
+    function the_name_not_be_greater_than_100_characters()
+    {
+        $userData = $this->userValidData(['name' => str_random(101)]);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['name']);
+    }
+
+    /** @test */
+    function the_first_name_is_required()
+    {
+        $userData = $this->userValidData(['first_name' => null]);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['first_name']);
+    }
+
+    /** @test */
+    function the_first_name_must_be_string()
+    {
+        $userData = $this->userValidData(['first_name' => 1234]);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['first_name']);
+    }
+
+    /** @test */
+    function the_first_name_not_be_greater_than_100_characters()
+    {
+        $userData = $this->userValidData(['first_name' => str_random(101)]);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['first_name']);
+    }
+
+    /** @test */
+    function the_last_name_is_required()
+    {
+        $userData = $this->userValidData(['last_name' => null]);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['last_name']);
+    }
+
+    /** @test */
+    function the_last_name_must_be_string()
+    {
+        $userData = $this->userValidData(['last_name' => 1234]);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['last_name']);
+    }
+
+    /** @test */
+    function the_last_name_not_be_greater_than_100_characters()
+    {
+        $userData = $this->userValidData(['last_name' => str_random(101)]);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['last_name']);
+    }
+
+    /** @test */
+    function the_email_is_required()
+    {
+        $userData = $this->userValidData(['email' => null]);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['email']);
+    }
+
+    /** @test */
+    function the_email_must_be_valid()
+    {
+        $userData = $this->userValidData(['email' => 'asdf@asdf']);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['email']);
+    }
+
+    /** @test */
+    function the_email_must_be_unique()
+    {
+        factory(User::class)->create(['email' => 'diego@diego.es' ]);
+        $userData = $this->userValidData(['email' => 'diego@diego.es']);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['email']);
+    }
+
+    /** @test */
+    function the_password_is_required()
+    {
+        $userData = $this->userValidData(['password' => null]);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['password']);
+    }
+
+    /** @test */
+    function the_password_must_be_confirmed()
+    {
+        $userData = $this->userValidData([
+            'password'              => 'asfas',
+            'password_confirmation' => null
+        ]);
+        $this->post(route('register'),$userData)->assertSessionHasErrors(['password']);
+    }
+
+    public function userValidData($override = []): array
+    {
+        return  array_merge(
+            [
+                'name' => 'Diego',
+                'first_name' => 'Diego',
+                'last_name' => 'Molina',
+                'email' => 'diego@diego.es',
+                'password' => 'secret',
+                'password_confirmation' => 'secret',
+            ],
+            $override
         );
     }
 }
